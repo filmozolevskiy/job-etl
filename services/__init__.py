c76273eddd2cf12d0c7391eb597fb57a2f7f2e34
filmozolevskiy <filippmozolevskiy@gmail.com>
2025-11-08 14:56:27 -0500

    docs: mention notifier microservice

--- services/__init__.py
+++ services/__init__.py
@@ -6,6 +6,7 @@ This package contains all microservices for the Job-ETL pipeline:
 - enricher: Enriches data with skills, location, etc.
 - ranker: Ranks jobs based on user preferences
 - publisher-hyper: Publishes to Tableau Hyper format
+- notifier: Sends webhook summaries and alerts
 """
 
 __version__ = "0.1.0"
